4d5777273495a8c73d00a5d354d466b26ce6f52b
Rosco Kalis <roscokalis@gmail.com>
2018-09-19 17:28:50 +0200

    [Lab3] Added basic Form generator (can't get it to work with Cnj/Dsj because of lists

--- Lab3/Rosco/Lab3.hs
+++ Lab3/Rosco/Lab3.hs
@@ -5,12 +5,11 @@ import Data.List
 import Data.Set
 import Data.Char
 import Test.QuickCheck
+import Control.Monad
+
 import Lecture3
 
 -- Assignment 1 (15:30-)
-toUniques :: Ord a => [a] -> [a]
-toUniques = toList . fromList
-
 tautology :: Form -> Bool
 tautology f = all (`evl` f) (allVals f)
 
@@ -24,3 +23,18 @@ equiv :: Form -> Form -> Bool
 equiv f g = tautology (Equiv f g)
 
 -- TODO Test and report
+
+formGen :: Int -> Gen Form
+formGen n
+  | n <= 0 = fmap Prop arbitrary
+  | n > 5 = formGen 5
+  | otherwise = oneof [
+      fmap Neg (formGen (n - 1)),
+      -- fmap Cnj [(formGen (n - 1)), (formGen (n - 2))],
+      -- fmap Dsj [(formGen (n - 1)), (formGen (n - 2))],
+      liftM2 Impl (formGen (n - 1)) (formGen (n - 2)),
+      liftM2 Equiv (formGen (n - 1)) (formGen (n - 2))]
+
+
+instance Arbitrary Form where
+  arbitrary = sized formGen
