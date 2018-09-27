71860e8009a2acd3f08410f9e05cfa99f1738d60
Rosco Kalis <roscokalis@gmail.com>
2018-09-27 13:38:10 +0200

    [Lab4] Added set intersect union and difference, plus some basic tests

--- Lab4/Rosco/Lab4.hs
+++ Lab4/Rosco/Lab4.hs
@@ -22,3 +22,27 @@ setGen n = fmap list2set arbitrary
 
 instance (Arbitrary a, Ord a) => Arbitrary (Set a) where
   arbitrary = sized setGen
+
+-- Assignment 3 (30 minutes)
+setLength :: Ord a => Set a -> Int
+setLength (Set xs) = length xs
+
+setIntersect :: Ord a => Set a -> Set a -> Set a
+setIntersect (Set a) (Set b) = list2set (a `intersect` b)
+
+setUnion :: Ord a => Set a -> Set a -> Set a
+setUnion (Set a) (Set b) = list2set (a `union` b)
+
+setDifference :: Ord a => Set a -> Set a -> Set a
+setDifference (Set a) (Set b) = list2set (a \\ b)
+
+-- TODO: Needs more test properties/testing
+
+testSetIntersect :: Ord a => Set a -> Set a -> Bool
+testSetIntersect a b = subSet i a && subSet i b where i = setIntersect a b
+
+testSetUnion :: Ord a => Set a -> Set a -> Bool
+testSetUnion a b = subSet a u && subSet b u where u = setUnion a b
+
+testSetDifference :: Ord a => Set a -> Set a -> Bool
+testSetDifference (Set a) (Set b) = all (not . (`elem` b)) d where (Set d) = setDifference (Set a) (Set b)
